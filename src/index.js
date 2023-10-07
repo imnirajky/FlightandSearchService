2da02bed9c225488660b3b688f0c5bcd6cdbffb9
Niraj Kumar <nirajkumar.123580@gmail.com>
2023-10-07 21:11:38 +0530

    Second Commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const dotenv = require('dotenv');
-dotenv.config();
-const app = express();
-console.log(process.env);
+const { PORT } = require('./config/serverConfig');
 
-app.listen(80, () => {
-    console.log('Server Launch', 80);
-});
+const setupAndStartServer = async() => {
+    //create the express object named app
+    const app = express();
+    app.listen(PORT, () => {
+        console.log(`Server Launched on PORT: ${PORT}`);
+    });
+}
+setupAndStartServer();
